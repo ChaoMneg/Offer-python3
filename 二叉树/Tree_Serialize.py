8c023c5d93299d9d97001de6d294429f40333a1b
ChaoMeng <mengchao0546@163.com>
2020-02-06 16:42:15 +0800

    Create Tree_Serialize.py

---
+++ 二叉树/Tree_Serialize.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/2/6/006 14:56
+# @Author  : mengchao
+# @Site    : 
+# @File    : Tree_Serialize.py
+# @Software: PyCharm
+
+"""
+题目：序列化二叉树
+请实现两个函数，分别用来序列化和反序列化二叉树
+
+二叉树的序列化是指：把一棵二叉树按照某种遍历方式的结果以某种格式保存为字符串，从而使得内存中建立起来的二叉树可以持久保存。
+序列化可以基于先序、中序、后序、层序的二叉树遍历方式来进行修改，序列化的结果是一个字符串，
+序列化时通过 某种符号表示空节点（#），以 ！ 表示一个结点值的结束（value!）。
+
+二叉树的反序列化是指：根据某种遍历顺序得到的序列化字符串结果str，重构二叉树。
+
+    	     8
+    	   /  \
+    	  6   10
+    	 / \  / \
+    	5  7 9 11
+序列化：8,6,5,#,7,#.10,9,#,11,#
+"""
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+class Solution:
+    # 序列化
+    def Serialize(self, root):
+        if root == None:
+            return "#"
+        return str(root.val) + "," + self.Serialize(root.left) + "," + self.Serialize(root.right)
+
+    # 反序列化
+    def Deserialize(self, s):
+        root, index = self.deserialize(s.split(","), 0)
+        return root
+
+    def deserialize(self,s, index):
+        if s[index] == "#":
+            index = index + 1
+            return None, index
+        root = TreeNode(int(s[index]))
+        index = index + 1
+        root.left, index = self.deserialize(s, index)
+        root.right, index = self.deserialize(s, index)
+        return root, index
+
+
+
