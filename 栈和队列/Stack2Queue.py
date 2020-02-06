80b49f386a50b6eb68fdee8e4b0923903a1fbb77
ChaoMeng <mengchao0546@163.com>
2020-02-06 16:50:52 +0800

    Update Stack2Queue.py

--- 栈和队列/Stack2Queue.py
+++ 栈和队列/Stack2Queue.py
@@ -4,7 +4,10 @@
 # @Site    : 
 # @File    : Stack2Queue.py
 # @Software: PyCharm
-
+"""
+题目：用两个栈实现队列
+用两个栈来实现一个队列，完成队列的Push和Pop操作。 队列中的元素为int类型。
+"""
 class Solution:
     def __init__(self):
         self.stack1 = []
